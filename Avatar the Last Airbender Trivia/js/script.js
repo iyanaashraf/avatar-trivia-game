c619d1656b246c60ebb4716afa15300bf6929deb
iyanaashraf <iyanaashraf@gmail.com>
2016-06-08 05:47:09 -0400

    updated all files

--- Avatar the Last Airbender Trivia/js/script.js
+++ Avatar the Last Airbender Trivia/js/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 	$('#begin-button').hide();
 	$('#dashboard').hide();
 	$('#question1-easy').hide();
+	$('#incorrect').hide();
 	
 
 	//user can start typing name immediently
@@ -27,26 +28,62 @@ $(document).ready(function() {
 		$('#begin-button').fadeIn(500);
 	});
 		
-$('#begin-button').on('click', function(e){
-	e.preventDefault();
-		var attrEasy = $('#easy').attr('checked');
-		var attrMedium = $('#medium').attr('checked');
-		var attrHard= $('#hard').attr('checked');
-			if(attr) {
+$('#begin-button').click(function(){
+
+		var easy = $('#easy').is(':checked');
+		var medium = $('#medium').is(':checked');
+		var hard = $('#hard').is(':checked');
+		
+
+			if(easy) {
 				console.log("player-chose-easy");
 				
-					
-					
-
+				$('#begin-button').fadeOut(1);
+				$('#level-screen').fadeOut(1);
+				$('#dashboard').fadeIn(1);
+				$('#question1-easy').fadeIn(500)
 				
+				$('#player-answer-1').click(function(){
+
+
+					var options  = $('#q1a, #q1b, #q1c').is(':checked');
+					var answer = $('#q1d').is(':checked');
+						
+						if (options) {
+							console.log("incorrect");
+							window.confirm("Oh no! That answer is incorrect! Try again.");
+								
+							
+
+
+						}
+						else {
+							console.log("correct");
+							window.confirm("Congratulations! That answer is correct!");
+							$('#player-answer-1').click(function(){
+								$('#next-button').fadeIn(500);
+							});
+
+						}
+
+
+
+				});
+				
+
+
 			}
-			else if(attrMedium) {
+			else if(medium) {
 				console.log("player-chose-medium");
 			}
 			else {
 				console.log("player-chose-hard");
 			}
+
+
+
 	});
+
 
 	
 	
